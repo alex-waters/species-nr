8604594fbf74352d52bf6c4fa6f8b9a7439d99ef
alex-waters <alexwaters613@gmail.com>
2025-11-10 18:26:52 +0000

    two field form working and writing out as pickle dict

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os
-from flask import Flask, render_template, send_from_directory, request
+import time, os, pickle
+from flask import Flask, render_template, send_from_directory, request, jsonify
 
 # instantiate app with static path to help PA distributed filesystem
 app = Flask(
@@ -23,6 +23,24 @@ def region_choice():
         page = render_template('layout.html')
 
     return page
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    return render_template('submit.html')
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        receive_time = round(time.time())
+        received = request.form.to_dict(flat=False)
+        pickle.dump(received, file=open('{}.p'.format(receive_time), 'wb'))
+    # with open('{}_form.json'.format(str(round(receive_time)), 'wb')) as f:
+    #     json.dump(request.form, f)
+
+    return render_template(
+        'thank_submit.html',
+    )
 
 
 if __name__ == '__main__':
